6a9628cbce8afa3ca6cd082245efbd64cfd27fef
Lemonnier Quentin <hello@quentinlemonnier.fr>
2021-06-15 14:17:14 +0200

    update navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,5 @@
 
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ActivationEnd } from '@angular/router';
-import { CartService } from 'src/app/cart.service';
-
 
 @Component({
   selector: 'app-navbar',
@@ -11,25 +8,13 @@ import { CartService } from 'src/app/cart.service';
 })
 export class NavbarComponent implements OnInit {
 
-  isBackBtn: boolean = false;
-
   constructor(
-    private route: ActivatedRoute,
-    private router: Router
+
   ) { 
 
   }
 
   ngOnInit(): void {
-    this.router.events.subscribe((val) => {
-      if (val instanceof ActivationEnd) {
-        if(val.snapshot.component['name'].toString() != 'CardsComponent'){
-          this.isBackBtn = true;
-        }else{
-          this.isBackBtn = false;
-        }
-      }
-   });
   }
 
 }
